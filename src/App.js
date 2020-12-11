49d57e83d75240388e390ab0adeaa0cf5d6630c9
omer <omersch381>
2020-12-11 14:33:01 +0200

    Add friend confirmation exchange
    This patch adds friend confirmation exchange, for now the exchange
    list is not popping the exchange after it is confirmed, but soon
    it will and that's the only issue with this feature.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import "./App.css";
 import web3 from "./web3";
 import profileAbi from "./ethereum/solidity/profile";
 
-const address = "0xa2Afe9750A328f27819DE513275466519A1Cb84f";
+const address = "0x010788BD86d55b073d1C995a05576B6cC502AC8E";
+
+// For testing purposes only!
+const friendsProfileAddress = "0x92D2ba8AC12435BF70F41eC311E269DbB8056E2c";
 
 const profile = new web3.eth.Contract(profileAbi, address);
 
@@ -11,137 +14,211 @@ class Test extends Component {
 
   state = {
     friendsAddress: "",
+    friendRequestIndex: "",
   };
 
   async componentDidMount() {
-    // const manager = await lottery.methods.manager().call();
-    // const players = await lottery.methods.getPlayers().call();
-    // const balance = await web3.eth.getBalance(lottery.options.address);
-
-    // let ethereum = window.ethereum;
-    // if (typeof ethereum !== "undefined") {
-    //   await ethereum.enable();
-    // }
-
-    // this.setState({ manager, players, balance });
+    let ethereum = window.ethereum;
+    if (typeof ethereum !== "undefined") {
+      await ethereum.enable();
+    }
   }
 
-  onSubmit = async (event) => {
+  // Remove Exchanges list for both our exchanges and friend exchanges
+  onRemoveExchangesList = async (event) => {
     event.preventDefault();
 
+    // Getting accounts list
     const accounts = await web3.eth.getAccounts();
 
-    console.log("here");
-    // const exchangeUniqueID = await profile.methods
-    // await profile.methods
-    //   .addFriendRequest("0x9668fde882D136BCBd549860a68a5e17fddD83d3")
-    //   .send({ from: accounts[0], gas: 1000000 });
-    const answer = await profile.methods.getAllExchanges().call();
-
-    console.log(answer[0].exchangeDetails);
-    // console.log(exchangeUniqueID);
-  };
-
-  onSubmitTest = async (event) => {
-    event.preventDefault();
-
-    const accounts = await web3.eth.getAccounts();
-
+    // Getting a reference to a friendsProfile
     const friendsProfile = new web3.eth.Contract(
       profileAbi,
       this.state.friendsAddress
     );
+
+    // Sending friend requests
+    await friendsProfile.methods
+      .removeAllExchanges()
+      .send({ from: accounts[0], gas: 1000000 });
+
+    let friendsExchanges = await friendsProfile.methods.getAllExchanges().call();
+
+    // Sending profile requests
+    await profile.methods
+      .removeAllExchanges()
+      .send({ from: accounts[0], gas: 1000000 });
+
+    const ourExchanges = await profile.methods.getAllExchanges().call();
+
+    console.log(ourExchanges);
+    console.log(friendsExchanges);
+  };
+
+
+
+  // Add a friend exchange for both our exchanges and friend exchanges
+  onSubmitTest = async (event) => {
+    event.preventDefault();
+
+    // Getting accounts list
+    const accounts = await web3.eth.getAccounts();
+
+    // Getting a reference to a friendsProfile
+    const friendsProfile = new web3.eth.Contract(
+      profileAbi,
+      this.state.friendsAddress
+    );
+
+    // Sending friend requests
     await friendsProfile.methods
       .addFriendRequestNotRestricted(address)
       .send({ from: accounts[0], gas: 1000000 });
 
+    let friendsExchanges = await friendsProfile.methods.getAllExchanges().call();
+
+    // Sending profile requests
     await profile.methods
       .addFriendRequest(this.state.friendsAddress)
       .send({ from: accounts[0], gas: 1000000 });
 
-    console.log("here passed requests");
-    const exchanges = await friendsProfile.methods.getAllExchanges().call();
-    console.log("here got his exchange");
     const ourExchanges = await profile.methods.getAllExchanges().call();
-    console.log("here got our exchange");
+
     console.log(ourExchanges);
-    console.log(exchanges);
+    console.log(friendsExchanges);
   };
 
-  handleChange = (event) => {
+
+
+
+  // Confirm a friend exchange for both our exchanges and friend exchanges
+  onConfirmFriendRequest = async (event) => {
+    event.preventDefault();
+
+    // Getting accounts list
+    const accounts = await web3.eth.getAccounts();
+
+    // Getting a reference to a friendsProfile
+    const friendsProfile = new web3.eth.Contract(
+      profileAbi,
+      this.state.friendsAddress
+    );
+
+    // Handling our exchanges:
+    await profile.methods
+      .confirmFriendRequest(this.state.friendRequestIndex) // The user presses a V button so we get the index from the user
+      .send({ from: accounts[0], gas: 1000000 });
+
+    // for testing
+    const ourExchanges = await profile.methods.getAllExchanges().call();
+    console.log("Making sure our exchangesList is empty:")
+    console.log(ourExchanges);
+
+    const ourfriends = await profile.methods.getFriends().call();
+    console.log("Making sure friendsList is not empty:")
+    console.log(ourfriends);
+
+    //Handling friend's exchanges:
+    let friendExchanges = await friendsProfile.methods.getAllExchanges().call();
+
+    for (let index = 0; index < friendExchanges.length; index++) {
+      const friendExchange = friendExchanges[index];
+
+      // I print it for testing
+      console.log("Exchange purpose is:");
+      console.log(friendExchange.exchangePurpose);
+      console.log("Exchange source is:");
+      console.log(friendExchange.exchangeDetails.source); // my friend's source
+      //TODO: Switch "0" with something clearer, "0" represents addFriendRequest Enum
+      if (friendExchange.exchangePurpose == "0" && friendExchange.exchangeDetails.source == this.state.friendsAddress) {
+        console.log("success with matching a friend's request");
+        await friendsProfile.methods
+          .confirmFriendRequest(index)
+          .send({ from: accounts[0], gas: 1000000 });
+      } else {
+        console.log(index + "is not a match");
+      }
+    }
+    // testing
+    friendExchanges = await friendsProfile.methods.getAllExchanges().call();
+    console.log("friends current exchanges:(supposed to be empty)");
+    console.log(friendExchanges);
+    console.log("friends current friendsList:(supposed to be not empty)");
+    const friendsCurrentFriendsList = await friendsProfile.methods.getFriends().call();
+    console.log(friendsCurrentFriendsList);
+
+
+    // await friendsProfile.methods
+    //   .confirmFriendRequestNotRestricted()
+    //   .send({ from: accounts[0], gas: 1000000 });
+
+    // const friendsExchanges = await friendsProfile.methods.getAllExchanges().call();
+
+    // // Sending profile requests
+    // await profile.methods
+    //   .removeAllExchanges()
+    //   .send({ from: accounts[0], gas: 1000000 });
+
+    // ourExchanges = await profile.methods.getAllExchanges().call();
+
+    // console.log(ourExchanges);
+    // console.log(friendsExchanges);
+  };
+
+  handleChangeFriendAddress = (event) => {
     event.preventDefault();
     this.setState({ friendsAddress: event.target.value });
   };
-  //   onSubmit = async (event) => {
-  //     event.preventDefault();
 
-  //     const accounts = await web3.eth.getAccounts();
-
-  //     this.setState({ message: "Waiting on transaction success..." });
-
-  //     await lottery.methods.enter().send({
-  //       from: accounts[0],
-  //       value: web3.utils.toWei(this.state.value, "ether"),
-  //     });
-
-  //     this.setState({ message: "you have been entered!" });
-  //   };
-
-  //   onClick = async () => {
-  //     const accounts = await web3.eth.getAccounts();
-
-  //     this.setState({ message: "Waiting on transaction success..." });
-
-  //     await lottery.methods.pickWinner().send({
-  //       from: accounts[0],
-  //     });
-
-  //     this.setState({ message: "A winner has been picked!" });
-  //   };
+  handleChangeFriendRequestIndex = (event) => {
+    event.preventDefault();
+    this.setState({ friendRequestIndex: event.target.value });
+  };
 
   render() {
     return (
       <div>
         <h2>Lottery Contract</h2>
-        <p>
-          {/* This contract is managed by {this.state.manager}. There are currently{" "}
-          {this.state.players.length} people entered, competing to win{" "}
-          {web3.utils.fromWei(this.state.balance, "ether")} ether. */}
-        </p>
-
         <hr />
-
-        <form onSubmit={this.onSubmit}>
-          <h4>Want to try your luck?</h4>
-
-          <div>
-            <label>Amount of ether to enter</label>
-            {/* <input
-              value={this.state.value}
-              onChange={(event) => this.setState({ value: event.target.value })}
-            /> */}
-          </div>
-          <button>Enter</button>
-        </form>
-        <hr />
-
-        <h4>Ready to pick a winner?</h4>
-        {/* <button onClick={this.onClick}>Pick a winner!</button> */}
-
-        <hr />
-        {/* <h1>{this.state.message}</h1> */}
         <form onSubmit={this.onSubmitTest}>
           <label>
             Add friend address:
             <input
               type="text"
               value={this.state.friendsAddress}
-              onChange={this.handleChange}
+              onChange={this.handleChangeFriendAddress}
               name="name"
             />
           </label>
           <input type="submit" value="Submit" />
         </form>
+
+        <form onSubmit={this.onRemoveExchangesList}>
+          <label>
+            Remove Exchanges list:
+            <input
+              type="text"
+              value={this.state.friendsAddress}
+              onChange={this.handleChangeFriendAddress}
+              name="name"
+            />
+          </label>
+          <input type="submit" value="Remove" />
+        </form>
+
+        <form onSubmit={this.onConfirmFriendRequest}>
+          <label>
+            Confirm a friend request: In the Application you will have to click V or something:
+            <input
+              type="text"
+              value={this.state.friendRequestIndex}
+              onChange={this.handleChangeFriendRequestIndex}
+              name="name"
+            />
+          </label>
+          <input type="submit" value="Confirm" />
+        </form>
+
       </div>
     );
   }
