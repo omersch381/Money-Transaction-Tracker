98c5babbaba08dac3d4b4fddde9b9f5a776dfdc6
omer <omersch381>
2020-12-16 21:20:33 +0200

    Add BinaryContract implementation
    This patch adds the BinaryContract implementaion.
    It is not tested yet, but its logic seems to work for the standard
    use case.

---
+++ src/solidity/contracts/BinaryContract.sol
@@ -0,0 +1,109 @@
+pragma solidity ^0.4.17;
+
+contract BinaryContract{
+
+    struct Transaction {
+        address from;
+        address to;
+        uint amount;
+        uint date;
+    }
+
+    // We have that struct as it stores the current contract status (the "sum" of all the contract's transactions)
+    // It saves power/money for the caller - every time a user adds a debt - it changes the ContractDebt as well
+    struct ContractDebt{
+        address debtor;
+        address creditor;
+        uint amountOwned;
+    }
+
+    uint creationDate;
+    uint validityInDays;
+    bool isValid = true;
+    address playerOne;
+    address playerTwo;
+    ContractDebt currentDebt;
+
+    Transaction[] binContractTransactionsLog;
+
+    // E.g. BinaryContract(player1, 2, player2) == deploy a new contract, where player 1 owes 2 shekels to player 2
+    function BinaryContract(address providedPlayerOne, uint amount, address providedPlayerTwo, uint providedValidityInDays) public  {
+        creationDate = block.timestamp;
+
+        if (providedValidityInDays == 0)
+            validityInDays = 365;
+        else
+            validityInDays = providedValidityInDays;
+
+        playerOne = providedPlayerOne;
+
+        playerTwo = providedPlayerTwo;
+
+        addTransaction(providedPlayerOne, amount, providedPlayerTwo);
+    }
+
+    function addTransaction (address sender, uint amount, address receiver) public ifValid {
+        Transaction memory transaction = Transaction({
+            from: sender,
+            to: receiver,
+            amount: amount,
+            date: block.timestamp
+        });
+
+        binContractTransactionsLog.push(transaction);
+
+        updateContractDebt(sender, amount, receiver);
+    }
+
+    function updateContractDebt (address sender, uint amount, address receiver) public {
+        if(binContractTransactionsLog.length != 1){ // in the constructor we just pushed the only transaction
+            if (currentDebt.debtor != sender){ // means the debtor now in a bigger debt
+            currentDebt.amountOwned += amount;
+            } else { // means the debtor is the sender
+                if (amount > currentDebt.amountOwned){
+                     updateDebtor(receiver);
+                     updateCreditor(sender);
+                     currentDebt.amountOwned =  amount - currentDebt.amountOwned;
+                } else{
+                    currentDebt.amountOwned -= amount;
+                }
+            }
+        } else {
+            currentDebt.debtor = receiver;
+            currentDebt.creditor = sender;
+            currentDebt.amountOwned = amount;
+        }
+    }
+
+    function updateDebtor(address newDebtor) public{
+        currentDebt.debtor = newDebtor;
+    }
+
+    function updateCreditor(address newCreditor) public{
+        currentDebt.creditor = newCreditor;
+    }
+
+    function getCurrentDebtorAddress() public view returns(address){
+        return currentDebt.debtor;
+    }
+
+    // function getCurrentDebtorName() public view returns(string memory){
+    //     //return currentDebt.debtor name
+    // }
+
+    function getCurrentCreditorAddress() public view returns(address){
+        return currentDebt.creditor;
+    }
+
+    // function getCurrentCreditorName() public view returns(string memory){
+    //     //return currentDebt.creditor name
+    // }
+
+    function getCurrentDebtAmount() public view returns(uint){
+        return currentDebt.amountOwned;
+    }
+
+    function getCurrentDebt() public view returns(ContractDebt memory){
+        return currentDebt;
+    }
+}
